bf15f895782e2367869a9f2d4b8cdccf4507b400
José Carlos Chávez <jcchavezs@gmail.com>
2017-06-14 20:09:16 +0200

    Adds format constants to the Tracer.

--- src/OpenTracing/Tracer.php
+++ src/OpenTracing/Tracer.php
@@ -2,11 +2,25 @@
 
 namespace OpenTracing;
 
+use OpenTracing\Carriers\HttpHeaders;
+use OpenTracing\Carriers\TextMap;
 use OpenTracing\Propagators\Reader;
 use OpenTracing\Propagators\Writer;
 
 interface Tracer
 {
+    const FORMAT_BINARY = 1;
+
+    /**
+     * @see HttpHeaders
+     */
+    const FORMAT_TEXT_MAP = 2;
+
+    /**
+     * @see TextMap
+     */
+    const FORMAT_HTTP_HEADERS = 3;
+
     /**
      * @param string $operationName
      * @param SpanReference|null $parentReference
@@ -31,13 +45,13 @@ interface Tracer
 
     /**
      * @param SpanContext $spanContext
-     * @param string $format
+     * @param int $format
      * @param Writer $carrier
      */
     public function inject(SpanContext $spanContext, $format, Writer $carrier);
 
     /**
-     * @param string $format
+     * @param int $format
      * @param Reader $carrier
      * @return SpanContext
      */
@@ -48,9 +62,12 @@ interface Tracer
      *
      * This method might not be needed depending on the tracing implementation
      * but one should make sure this method is called after the request is finished.
-     * As an implementor, a good idea would be to use an asynchronous {@ see message bus}
-     * or use the call to {@see fastcgi_finish_request} in order to not to delay the end
-     * of the request to the final user.
+     * As an implementor, a good idea would be to use an asynchronous message bus
+     * or use the call to fastcgi_finish_request in order to not to delay the end
+     * of the request to the client.
+     *
+     * @see fastcgi_finish_request()
+     * @see https://www.google.com/search?q=message+bus+php
      */
     public function flush();
 }
