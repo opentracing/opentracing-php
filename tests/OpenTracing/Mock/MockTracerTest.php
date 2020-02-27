cfafbb284bba99fc0eea8c6f805a0956b54c9c9c
Christian Alexander Wolf <mail@cawolf.de>
2020-02-27 21:49:29 +0100

    chore: add test case

--- tests/OpenTracing/Mock/MockTracerTest.php
+++ tests/OpenTracing/Mock/MockTracerTest.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace OpenTracing\Mock\Tests;
+namespace OpenTracing\Tests\Mock;
 
 use OpenTracing\Exceptions\UnsupportedFormat;
 use OpenTracing\Mock\MockTracer;
 use OpenTracing\NoopSpanContext;
+use OpenTracing\Reference;
 use OpenTracing\SpanContext;
 use PHPUnit\Framework\TestCase;
 
@@ -32,6 +33,20 @@ final class MockTracerTest extends TestCase
         $activeSpan = $tracer->getActiveSpan();
 
         $this->assertNull($activeSpan);
+    }
+
+    public function testStartSpanWithReference(): void
+    {
+        $tracer = new MockTracer();
+        $tracer->startSpan('parent_name');
+        $parentSpan = $tracer->getSpans()[0];
+        $tracer->startSpan(
+            self::OPERATION_NAME,
+            ['references' => [Reference::create(Reference::CHILD_OF, $parentSpan)]]
+        );
+        $activeSpan = $tracer->getActiveSpan();
+
+        self::assertNull($activeSpan);
     }
 
     public function testInjectWithNoInjectorsFails()
